d24f98b3c2414dc3115f77e47eaba3f298dcb30f
DESKTOP-P4UUKAO\student <bert0210au@gmail.com>
2025-08-16 14:24:20 +0800

    修正updateProductStatus

--- src/main/java/com/giun/ecs/controller/ProductController.java
+++ src/main/java/com/giun/ecs/controller/ProductController.java
@@ -84,4 +84,15 @@ public class ProductController {
     return ResponseEntity.ok(resp);
   }
 
+  /**
+   * 刪除產品
+   * 
+   * @param id 商品ID
+   * @return
+   */
+  @PutMapping("/deleteProduct/{id}")
+  public ResponseEntity<Outbound> deleteProduct(@PathVariable("id") Integer id) {
+    Outbound resp = productService.deleteProduct(id);
+    return ResponseEntity.ok(resp);
+  }
 }
--- src/main/java/com/giun/ecs/entity/Product.java
+++ src/main/java/com/giun/ecs/entity/Product.java
@@ -69,4 +69,10 @@ public class Product extends BaseEntity {
    */
   @Column(name = "image_type")
   private String imageType;
+
+  /**
+   * 商品狀態
+   */
+  @Column(name = "status", nullable = false)
+  private String status;
 }
