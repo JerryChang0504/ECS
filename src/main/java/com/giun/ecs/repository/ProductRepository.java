6f295f77edbeb28ad4d0bdbe04af00ef28cfcc22
Wei-chih-han <weidan26504@gmail.com>
2025-08-16 14:11:47 +0800

    修正

--- src/main/java/com/giun/ecs/repository/ProductRepository.java
+++ src/main/java/com/giun/ecs/repository/ProductRepository.java
@@ -1,11 +1,18 @@
 package com.giun.ecs.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import com.giun.ecs.entity.Product;
+
+import jakarta.transaction.Transactional;
 
 public interface ProductRepository extends JpaRepository<Product, Integer> {
 
-@Query(value = "UPDATE product SET states = :states WHERE id = :id")
-    void updateProductStatus(Integer id, String states);
+@Modifying
+    @Transactional
+    @Query("UPDATE Product p SET p.states = :status WHERE p.id = :id")
+    void updateProductStatus(@Param("id") Integer id,
+            @Param("status") String status);
 }
