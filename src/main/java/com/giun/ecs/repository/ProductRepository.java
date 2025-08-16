7954f9a4b5c2b03912f1282959569170d6d79734
JerryChang0504 <fx2ptt@gmail.com>
2025-08-16 12:27:20 +0800

    新增刪除API

--- src/main/java/com/giun/ecs/repository/ProductRepository.java
+++ src/main/java/com/giun/ecs/repository/ProductRepository.java
@@ -1,8 +1,13 @@
 package com.giun.ecs.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
 import com.giun.ecs.entity.Product;
 
 public interface ProductRepository extends JpaRepository<Product, Integer> {
+
+    @Query(value = "UPDATE product SET states = :states WHERE id = :id")
+    void updateProductStatus(Integer id, String states);
 
 }
