f2e4c1bf0e56a412db9e5dc670e7fd53264cb491
JerryChang0504 <fx2ptt@gmail.com>
2025-08-30 02:18:00 +0800

    修正 ApplicationException

--- src/main/java/com/giun/ecs/exception/ApplicationException.java
+++ src/main/java/com/giun/ecs/exception/ApplicationException.java
@@ -1,18 +1,25 @@
 package com.giun.ecs.exception;
 
 import com.giun.ecs.enums.ResultCode;
-
 import lombok.Data;
 
 @Data
 public class ApplicationException extends Exception {
 
-	private String code;
-	private String msg;
+  private String code;
+  private String msg;
 
-	public ApplicationException(ResultCode resultCode) {
-		super(resultCode.getMsg());
-		this.code = resultCode.getCode();
-		this.msg = resultCode.getMsg();
-	}
+  public ApplicationException(
+      ResultCode resultCode) {
+    super(resultCode.getMsg());
+    this.code = resultCode.getCode();
+    this.msg = resultCode.getMsg();
+  }
+
+  public ApplicationException(
+      String msg) {
+    super(msg);
+    this.code = ResultCode.FAIL.getCode();
+    this.msg = msg;
+  }
 }
