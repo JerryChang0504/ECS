8b5fdb91870b44faf1135b834089a4448b1a7b62
JerryChang0504 <fx2ptt@gmail.com>
2025-08-30 15:39:41 +0800

    allCategories 進行排序

--- src/main/java/com/giun/ecs/service/CategoriesServiceImpl.java
+++ src/main/java/com/giun/ecs/service/CategoriesServiceImpl.java
@@ -1,5 +1,6 @@
 package com.giun.ecs.service;
 
+import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -41,6 +42,8 @@ public class CategoriesServiceImpl implements CategoriesService {
     @Override
     public Outbound allCategories() throws Exception {
         List<OptionResp> result = categoriesRepository.findAll().stream()
+                .sorted(Comparator.comparing(Categories::getListName)
+                        .thenComparing(Categories::getSortOrder))
                 .map(Categories -> {
                     return OptionResp.builder()
                             .id(Categories.getId())
