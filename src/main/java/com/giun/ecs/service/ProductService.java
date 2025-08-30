31668750fa18204894800dbbcb1d45d60b2f38dc
JerryChang0504 <fx2ptt@gmail.com>
2025-08-30 16:12:46 +0800

    getProductById methid 新增states欄位

--- src/main/java/com/giun/ecs/service/ProductService.java
+++ src/main/java/com/giun/ecs/service/ProductService.java
@@ -39,14 +39,14 @@ public class ProductService {
   }
 
   public Outbound getProductById(Integer id) {
-    Product product =
-        productRepository.findById(id).orElseThrow(() -> new RuntimeException("Product not found"));
+    Product product = productRepository.findById(id).orElseThrow(() -> new RuntimeException("Product not found"));
 
     ProductResp response = ProductResp.builder()
         .id(product.getId())
         .name(product.getName())
         .price(product.getPrice())
         .stock(product.getStock())
+        .states(product.getStates())
         .description(product.getDescription())
         .category(product.getCategory())
         .imageBase64(generateImageBase64(product.getImageData(), product.getImageType()))
@@ -75,8 +75,7 @@ public class ProductService {
   }
 
   public Outbound updateProduct(Integer id, ProductUploadReq req) {
-    Product product =
-        productRepository.findById(id).orElseThrow(() -> new RuntimeException("Product not found"));
+    Product product = productRepository.findById(id).orElseThrow(() -> new RuntimeException("Product not found"));
 
     ImageInfo imageInfo = processBase64Image(req.getImageBase64(), req.getImageType());
 
@@ -144,7 +143,7 @@ public class ProductService {
   /**
    * 處理 Base64 圖片字串，解析出圖片二進制資料和類型。
    * 
-   * @param base64String Base64 編碼的圖片字串，可包含 Data URI 前綴。
+   * @param base64String      Base64 編碼的圖片字串，可包含 Data URI 前綴。
    * @param existingImageType 已知或預設的圖片類型。
    * @return 包含圖片資料和類型的 ImageInfo 物件。
    */
