14460c10390dcc199d03a7cee0f5783b7c122031
JerryChang0504 <fx2ptt@gmail.com>
2025-08-30 11:43:48 +0800

    新增 updateOption API    @PutMapping("/update/{id}")

--- src/main/java/com/giun/ecs/service/CategoriesServiceImpl.java
+++ src/main/java/com/giun/ecs/service/CategoriesServiceImpl.java
@@ -62,4 +62,24 @@ public class CategoriesServiceImpl implements CategoriesService {
         return Outbound.ok("Category deleted successfully");
     }
 
+    @Override
+    public Outbound updateCategorie(Integer id, AddOptionReq req) throws Exception {
+        Categories categorie = categoriesRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Category not found"));
+
+        Categories updateCategories = Categories.builder()
+                .id(categorie.getId())
+                .listName(req.getListName())
+                .name(req.getName())
+                .value(req.getValue())
+                .sortOrder(req.getSortOrder())
+                .isActive(req.getIsActive())
+                .description(req.getDescription())
+                .build();
+
+        categoriesRepository.save(updateCategories);
+
+        return Outbound.ok("Category updated successfully");
+    }
+
 }
