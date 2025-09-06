f2b1aa60bd5f8266db67b2d9b184dc2bc28d40fe
Wei-chih-han <weidan26504@gmail.com>
2025-09-06 15:23:55 +0800

    建立login

---
+++ src/main/java/com/giun/ecs/dto/response/LoginResp.java
@@ -0,0 +1,12 @@
+package com.giun.ecs.dto.response;
+
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+public class LoginResp {
+
+    private String token;
+    private String role;
+}
