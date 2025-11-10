b87fe9f6f683710f6126601ee4e7e72d4f847a7a
GOTO123978 <a0981640016@gmail.com>
2025-11-10 21:56:09 +0800

    add deleteProduct api

--- src/main/java/com/giun/ecs/controller/ProductController.java
+++ src/main/java/com/giun/ecs/controller/ProductController.java
@@ -82,4 +82,15 @@ public class ProductController {
     return ResponseEntity.ok(response);
   }
 
+  /**
+   * 刪除產品
+   * 
+   * @param id 商品ID
+   * @return
+   */
+  @PutMapping("/deleteProducts/{id}")
+  public ResponseEntity<Outbound> deleteProduct(@PathVariable("id") Integer id) throws Exception {
+    Outbound response = productService.deleteProduct(id);
+    return ResponseEntity.ok(response);
+  }
 }
