0d8e2312e466bff262bc17de7c148f71a4987868
Jerry Chang <fx2ptt@gmail.com>
2025-11-14 19:55:42 +0800

    新增 /options/add API

--- src/main/java/com/giun/ecs/service/OptionlistService.java
+++ src/main/java/com/giun/ecs/service/OptionlistService.java
@@ -5,9 +5,12 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.giun.ecs.dto.request.AddOptionReq;
 import com.giun.ecs.dto.response.Outbound;
 import com.giun.ecs.entity.OptionsList;
 import com.giun.ecs.repository.OptionlistRepository;
+
+import jakarta.transaction.Transactional;
 
 @Service
 public class OptionlistService {
@@ -21,5 +24,24 @@ public class OptionlistService {
     public Outbound getOptions() {
         List<OptionsList> optionlist = optionlistRepository.findAll();
         return Outbound.ok(optionlist);
+    }
+
+    /**
+     * 新增選項
+     * 
+     * @param req
+     */
+    @Transactional
+    public Outbound addOptions(AddOptionReq req) throws Exception {
+        OptionsList option = OptionsList.builder()
+                .listName(req.getListName())
+                .name(req.getName())
+                .value(req.getValue())
+                .sortOrder(req.getSortOrder())
+                .isActive(req.getIsActive())
+                .description(req.getDescription())
+                .build();
+        optionlistRepository.save(option);
+        return Outbound.ok();
     }
 }
