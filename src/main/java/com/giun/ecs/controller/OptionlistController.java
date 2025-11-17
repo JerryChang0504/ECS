6e9a8d2d28e4a32ea2cc2cb3d19500cffc6bfa14
Jerry Chang <fx2ptt@gmail.com>
2025-11-17 19:28:26 +0800

    新增 更新選項API

--- src/main/java/com/giun/ecs/controller/OptionlistController.java
+++ src/main/java/com/giun/ecs/controller/OptionlistController.java
@@ -3,12 +3,15 @@ package com.giun.ecs.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.giun.ecs.dto.request.AddOptionReq;
+import com.giun.ecs.dto.request.EditOptionReq;
 import com.giun.ecs.dto.response.Outbound;
 import com.giun.ecs.service.OptionlistService;
 
@@ -38,6 +41,19 @@ public class OptionlistController extends BaseController {
     public ResponseEntity<Outbound> addOptions(@RequestBody AddOptionReq req) throws Exception {
         checkRequired(req.getListName(), req.getName(), req.getValue());
         Outbound response = optionlistService.addOptions(req);
+        return ResponseEntity.ok(response);
+    }
+
+    /**
+     * 更新選項
+     * 
+     * @throws Exception
+     */
+    @PutMapping("edit/{id}")
+    public ResponseEntity<Outbound> editOptions(@PathVariable Integer id, @RequestBody EditOptionReq req)
+            throws Exception {
+        Outbound response = optionlistService.editOptions(id, req);
+
         return ResponseEntity.ok(response);
     }
 
